45a488eb7c1561b763f1fc4bbae6a6f377b76e28
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2021-11-25 22:21:38 +0500

    excersice 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -74,6 +74,13 @@ function useUser() {
 }
 
 // 🐨 add a function here called `updateUser`
+function updateUser(userDispatch, user, updates) {
+  userDispatch({type: 'start update', updates: updates})
+  userClient.updateUser(user, updates).then(
+    updatedUser => userDispatch({type: 'finish update', updatedUser}),
+    error => userDispatch({type: 'fail update', error}),
+  )
+}
 // Then go down to the `handleSubmit` from `UserSettings` and put that logic in
 // this function. It should accept: dispatch, user, and updates
 
@@ -98,11 +105,7 @@ function UserSettings() {
   function handleSubmit(event) {
     event.preventDefault()
     // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updateUser(userDispatch, user, formState)
   }
 
   return (
